ebe1e863c6af969a610f47bf95b10975ed0ca156
John Wonson <jmwonson@hotmail.com>
2018-01-02 18:01:51 +1100

    Minecraft -Esque VoxGen

---
+++ Assets/Scripts/Modify.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace VoxelGeneration
+{
+    public class Modify : MonoBehaviour
+    {
+        public Camera cam;
+
+        Ray ray;
+        
+        // Update is called once per frame
+        void Update ()
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Input.GetMouseButton(0))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    EditTerrain.SetBlock(hit, new BlockAir());
+                }
+            }
+        }
+    }
+}
